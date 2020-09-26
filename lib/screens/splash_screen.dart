76b8fd570ecbae581e4ed47d58d0d4fdf669e2dd
Ab3pAydAy <salinas772089@yahoo.com>
2020-09-25 23:11:13 -0500

    splash screen added

---
+++ lib/screens/splash_screen.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+import 'dart:async';
+import 'package:habittracker/screens/home_data_screen.dart';
+import 'home_data_screen.dart';
+
+class SplashScreen extends StatefulWidget {
+  @override
+  _SplashScreenState createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+    startTime();
+  }
+
+  startTime() async {
+    var duration = new Duration(seconds: 2);
+    return new Timer(duration, navigateToDeviceScreen);
+  }
+
+  navigateToDeviceScreen() {
+    Navigator.pushReplacement(
+        context, MaterialPageRoute(builder: (context) => Homedatascreen()));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: Container(
+      decoration: new BoxDecoration(color: Colors.white),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          new Text("AMOTION",
+              style: TextStyle(
+                  fontSize: 58,
+                  color: Colors.cyan[400],
+                  fontWeight: FontWeight.w600)),
+          new Text('Great Coffee at Home',
+              style: TextStyle(
+                fontSize: 22,
+                color: Colors.white,
+              ))
+        ],
+      ),
+      alignment: Alignment(0.10, 1.0),
+    ));
+  }
+}
